da535ae3dced1fe131d2a97cadb3ea699b66c8af
Lior Haim <lior@dotcore.co.il>
2020-04-13 15:46:55 +0000

    Refactor promises structure to async/await
    Signed-off-by: Lior Haim <lior@dotcore.co.il>

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 import fetch from 'node-fetch';
 import xml2js from 'xml2js';
 
-const body = {
-  DeviceType: 1,
-  Email: '',
-  ApplicationVersion: '1.6.1.34.1',
-  Pass: '',
-};
+const baseUrl = 'http://mobile.icast.co.il/';
 
-const params = new URLSearchParams();
-for(const key in body) {
-  params.append(key, body[key]);
+function createParamsString(obj) {
+  const params = new URLSearchParams();
+  for(const key in obj) {
+    params.append(key, obj[key]);
+  }
+  return params;
 }
-
-const options = {
-  method: 'POST',
-  body: params,
-};
 
 function parseXmlObject(obj) {
   const {key, string} = obj.plist.array[0].dict[0];
@@ -27,10 +20,29 @@ function parseXmlObject(obj) {
   return res;
 }
 
-fetch('http://mobile.icast.co.il/BookUserLogin.aspx', options)
-.then(res => res.text())
-  .then(res => xml2js.parseStringPromise(res))
-  .then(res => {
-    const obj = parseXmlObject(res);
-    console.table(JSON.stringify(obj));
-  });
+async function sendRequest(page, params) {
+
+  const options = {
+    method: 'POST',
+    body: createParamsString(params),
+  };
+
+  const url = `${baseUrl}${page}`;
+  const res = await fetch(url, options);
+  const xmlStr = await res.text();
+  const json = await xml2js.parseStringPromise(xmlStr);
+  const obj = parseXmlObject(json);
+  return obj;
+}
+
+const params = {
+  DeviceType: 1,
+  Email: '',
+  ApplicationVersion: '1.6.1.34.1',
+  Pass: '',
+};
+
+(async function Run() {
+  const a = await sendRequest('BookUserLogin.aspx', params);
+  console.log(a);
+})();
